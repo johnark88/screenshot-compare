2177305e64c0ade7d86862ae1cf25e801d8c8854
johnark88 <john.arkema@gmail.com>
2020-04-22 09:26:58 -0700

    lint settings - remove vue stub data

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,13 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'max-len': ['warn', { code: 100 }],
+    eqeqeq: ['warn', 'smart'],
+    'no-empty-function': 'error',
+    'no-multi-spaces': 'error',
+    'no-param-reassign': 'error',
+    'arrow-body-style': ['error', 'as-needed'],
   },
   overrides: [
     {
