a9125f00457004879b7fa5d9f0d77424903dd1f1
johnark88 <john.arkema@gmail.com>
2020-04-28 13:36:14 -0700

    Router with requires Auth meta

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,48 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+const firebase = require('../../fireConfig.js');
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
+  // {
+  //   path: '*',
+  //   redirect: '/login'
+  // },
   {
     path: '/',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/contact',
+    name: 'Contact',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Contact.vue'),
+    meta: { requiresAuth: true }
   }
-]
+  // login, signUp, dashboard,settings,support,contact
+];
 
 const router = new VueRouter({
+  mode: 'history',
   routes
-})
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  const currentUser = firebase.auth.currentUser;
+
+  if (requiresAuth && !currentUser) {
+    next('login');
+  } else if (requiresAuth && currentUser) {
+    next();
+  } else {
+    next();
+  }
+});
+
+export default router;
